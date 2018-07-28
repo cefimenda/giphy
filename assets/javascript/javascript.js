3cb449529e8095d434c7c8e72025f092c59786c9
cefimenda <jeffymenda@gmail.com>
2018-07-28 10:24:46 -0400

    favorites persist correctly, also changes in UI regarding favorites

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,8 @@
-gifList = {}
-likedGifs = {}
-searchHistory = []
+var gifList = JSON.parse(localStorage.getItem('gifList')) || {}
+var likedGifs = JSON.parse(localStorage.getItem('likedGifs')) || {}
+var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || []
+var displayedGifs = {}
+
 
 async function gifSearch(searchParameters) {
     var query = $.param(searchParameters)
@@ -13,8 +15,10 @@ async function gifSearch(searchParameters) {
         for (var i = 0; i < response.data.length; i++) {
             var gifObj = new Gif(response.data[i])
             dumpGif(gifObj)
-            gifObj.element = $(".displayGif").children().first()
+            gifObj.element = JSON.stringify($(".displayGif").children().first())
             gifList[gifObj.id] = gifObj
+            displayedGifs[gifObj.id] = gifObj
+            localStorage.setItem('gifList', JSON.stringify(gifList))
         }
     })
 }
@@ -27,6 +31,18 @@ var parameters = {
 }
 
 $(function () {
+    if (searchHistory.length > 0) {
+        console.log('something already stored')
+        createHistoryBar()
+        getHistoryButtons()
+    }
+    if (likedGifs != null) {
+        console.log('something already liked')
+        $(".faves").empty()
+        for (var i in likedGifs) {
+            dumpGif(likedGifs[i], $(".faves"))
+        }
+    }
     $("#searchButton").click(function () {
         event.preventDefault()
         parameters.q = $("#gifQuery").val().trim()
@@ -36,10 +52,11 @@ $(function () {
         searchHistory.push(parameters.q)
         createHistoryBar()
         getHistoryButtons()
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
     });
     $(".displayGif").on('click', '.close', function () {
         var id = $(this).parent().parent().attr("id")
-        delete gifList[id]
+        delete displayedGifs[id]
         $(this).parent().parent().remove()
     });
     $(".displayGif").on('mouseenter', '.heart', function () {
@@ -60,32 +77,11 @@ $(function () {
         }
     });
     $(".displayGif").on('click', '.heart', function () {
-        var id = $(this).parent().parent().attr('id')
-        if (gifList[id].liked == true) {
-            gifList[id].liked = false
-
-            delete likedGifs[id]
-        } else {
-            gifList[id].liked = true
-            likedGifs[id] = gifList[id]
-            return
-        }
+        heartButtonClick(this)
     });
     $("#favoritesButton").click(function () {
         event.preventDefault()
-        if ($(this).text() == "Show Favorites") {
-            $(".displayGif").empty()
-            for (var i in likedGifs) {
-                $('.displayGif').prepend(likedGifs[i].element)
-            }
-            $(this).text('Show All')
-        } else {
-            $(".displayGif").empty()
-            for (var i in gifList) {
-                $('.displayGif').prepend(gifList[i].element)
-            }
-            $(this).text('Show Favorites')
-        }
+        $(".favoriteModal").modal('toggle')
     });
     $(".displayGif").on("click", ".card-img-top", function () {
         var id = $(this).parent().attr('id')
@@ -96,7 +92,19 @@ $(function () {
             $(this).attr('src', gifList[id].static)
         }
     });
-    $(".navbarRow").on("click",".historyButton",function(){
+    $(".faves").on("click", ".card-img-top", function () {
+        var id = $(this).parent().attr('id')
+        console.log(id)
+        if ($(this).attr('src') == gifList[id].static) {
+            $(this).attr('src', gifList[id].url)
+        } else {
+            $(this).attr('src', gifList[id].static)
+        }
+    });
+    $(".faves").on('click', '.heart', function () {
+        heartButtonClick(this)
+    });
+    $(".navbarRow").on("click", ".historyButton", function () {
         parameters.q = $(this).text()
         parameters.limit = $("#gifCount option:selected").val()
         $("#gifQuery").val("")
@@ -104,19 +112,64 @@ $(function () {
     });
 })
 
-function dumpGif(gifObj) {
-    for (var i in gifList) {
-        if (gifObj.id == gifList[i].id) { return }
+function heartButtonClick(that) {
+    var id = $(that).parent().parent().attr('id')
+    console.log(gifList[id].liked)
+    console.log
+    if (gifList[id].liked != true) {
+        console.log('liking')
+        gifList[id].liked = true
+        $(".faves").empty()
+        likedGifs[id] = gifList[id]
+        for (var i in likedGifs) {
+            console.log("dumping")
+            dumpGif(likedGifs[i], $(".faves"))
+        }
+        changeLikedButton('full', id)
+    } else {
+        console.log('disliking')
+        gifList[id].liked = false
+        delete likedGifs[id]
+        console.log('test')
+        $(".faves").empty()
+        for (var i in likedGifs) {
+            console.log("dumping")
+            dumpGif(likedGifs[i], $(".faves"))
+        }
+        changeLikedButton('empty', id)
     }
+
+    localStorage.setItem("likedGifs", JSON.stringify(likedGifs))
+    localStorage.setItem("gifList", JSON.stringify(gifList))
+
+}
+function changeLikedButton(targetStatus, id) {
+
+    for (var i = 0; i < $("." + id).length; i++) {
+        var element = $(($("." + id))[i])
+        element.children().first().children().last().attr('src', "assets/images/like-" + targetStatus + ".png")
+    }
+}
+function dumpGif(gifObj, target) {
+    //we must avoid duplicates in the display area
+    if (target == null || target == $(".displayGif"))
+        for (var i in displayedGifs) {
+            if (gifObj.id == displayedGifs[i].id) { return }
+        }
     var imageUrl = gifObj.static
     var titleText = gifObj.title
-    var container = $("<div>").addClass("card bg-light col-12 col-sm-6 col-md-4 col-lg-3 shadow-sm mx-auto p-0 m-sm-1")
+    if (target == null) {
+        var container = $("<div>").addClass("card bg-light col-12 col-sm-6 col-md-4 col-lg-3 shadow-sm mx-auto p-0 m-sm-1")
+    } else { //if adding to the faves section
+        var container = $("<div>").addClass("card bg-light shadow-sm my-2 mx-auto p-0")
+    }
     container.css({
         'width': '200px',
         'max-height': '350px',
         'overflow': 'scroll'
     });
     container.attr('id', gifObj.id)
+    container.addClass(gifObj.id)
     var img = $("<img>").attr("src", imageUrl)
     img.addClass("card-img-top")
     img.css({
@@ -146,7 +199,11 @@ function dumpGif(gifObj) {
     close.addClass("float-left ml-2 close")
     buttonBar.append(close)
 
-    var heart = $("<img>").attr('src', 'assets/images/like-empty.png');
+    if (gifObj.liked == true) {
+        var heart = $("<img>").attr('src', 'assets/images/like-full.png');
+    } else {
+        var heart = $("<img>").attr('src', 'assets/images/like-empty.png');
+    }
     heart.css({
         'width': '20px',
         'height': '20px'
@@ -156,7 +213,7 @@ function dumpGif(gifObj) {
 
     container.prepend(buttonBar)
 
-    var display = $(".displayGif")
+    var display = target || $(".displayGif")
     display.prepend(container)
 }
 
@@ -166,30 +223,29 @@ function Gif(gif) {
     this.static = gif.images['480w_still'].url
     this.url = gif.images.original.url;
     this.title = gif.title;
-    this.element = {
-    };
     this.rating = gif.rating
 }
 
-function createHistoryBar(){
+function createHistoryBar() {
     $(".historyBar").remove()
     $(".historyCol").remove()
     $(".dynamicPlaceholder").remove()
     $(".historyLine").remove()
 
     var col = $("<div>").addClass("col-12 historyCol")
-    col.append("<hr class ='historyLine>")
-    var div = $("<div>").addClass('w-100 historyBar mt-3 mb-0')
+    var line = $("<hr>").addClass("historyLine m-3")
+    col.append(line)
+    var div = $("<div>").addClass('w-100 historyBar mt-1 mb-0')
     div.text('Search History')
     col.append(div)
     $(".navbarRow").append(col)
-    var placeholder = $("<div>").addClass("w-100 mt-3 p-3 dynamicPlaceholder")
+    var placeholder = $("<div>").addClass("w-100 mt-5 p-3 dynamicPlaceholder")
     $("body").prepend(placeholder)
 }
 
-function getHistoryButtons(){
+function getHistoryButtons() {
     $(".historyBar").empty()
-    for (var i in searchHistory){
+    for (var i in searchHistory) {
         var but = $("<button>").addClass('btn-sm ml-3 btn-outline-secondary historyButton')
         but.text(searchHistory[i])
         $(".historyBar").prepend(but)
